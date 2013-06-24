c4268f939f1c876cc1a781392e352c7fdf75cc3f
Vignon Zinsou <vinzinsou@gmail.com>
2013-06-24 16:53:41 -0600

    Update count.py

--- count.py
+++ count.py
@@ -1,14 +1,20 @@
-def count(sequence, item):
+def count(sequence, item): # Long count function
     counter = 0
     for i in sequence:
         if i == item:
             counter+=1
     return counter
     
-def purify(numbers):
+def purify(numbers): # returns all even numbers in a list
     evens = []
     for i in numbers:
         if i%2 == 0:
             evens.append(i)
     return evens
+    
+def product(numbers): # returns the product of a list of int's 
+    product = 1
+    for i in numbers:
+        product*=i
+    return product
   
