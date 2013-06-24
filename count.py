88d96342f8f5c4c27a1400bf6ccb65d661572141
Vignon Zinsou <vinzinsou@gmail.com>
2013-06-24 15:48:53 -0700

    Update count.py
    Count (The scenic route)
    Purify (Typical Evens)

--- count.py
+++ count.py
@@ -4,4 +4,11 @@ def count(sequence, item):
         if i == item:
             counter+=1
     return counter
+    
+def purify(numbers):
+    evens = []
+    for i in numbers:
+        if i%2 == 0:
+            evens.append(i)
+    return evens
   
